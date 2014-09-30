3cc6a4ba62615c9b7471f2b6fa59c9ac4f5494d4
Matthew Dwyer <mdwyer223@aol.com>
2014-09-30 10:47:59 -0400

    Set up the camera class, along with the input class to be used for global checking of key presses

--- CKB/CKB/CKB/Game1.cs
+++ CKB/CKB/CKB/Game1.cs
@@ -19,35 +19,40 @@ namespace CKB
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        public enum GameState
+        {
+            PLAYING, PAUSE, MAIN
+        }
+
+
+        static GameState mainGameState = GameState.PLAYING;
+        public static GameState State
+        {
+            get { return mainGameState; }
+        }
+
+        static Camera2D camera;
+        public static Camera2D Camera
+        {
+            get { return camera; }
+        }
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
+            camera = new Camera2D(this);
+            Components.Add(camera);
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
         }
 
         /// <summary>
