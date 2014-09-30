57f9033863c590f3b3fba87540ab302b4beace8b
David Chung <davidc4747@yahoo.com>
2014-09-30 10:51:49 -0400

    basic start up stuff

--- CKB/CKB/CKB/Game1.cs
+++ CKB/CKB/CKB/Game1.cs
@@ -19,10 +19,24 @@ namespace CKB
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        static ContentManager otherContent;
+        public static ContentManager GameContent
+        {
+            get { return otherContent; }
+        }
+
+        static GraphicsDevice otherDevice;
+        public static Viewport View
+        {
+            get { return otherDevice.Viewport; }
+        }
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+            otherContent = new ContentManager(Content.ServiceProvider);
+            otherContent.RootDirectory = "Content";
         }
 
         /// <summary>
